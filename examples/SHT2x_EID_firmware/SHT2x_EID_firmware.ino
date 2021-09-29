3c3c519d47ef42d13ffbda50e76c233dba831b9f
rob tillaart <rob.tillaart@gmail.com>
2021-09-29 13:19:36 +0200

    Si70xx test

---
+++ examples/SHT2x_EID_firmware/SHT2x_EID_firmware.ino
@@ -0,0 +1,48 @@
+//
+//    FILE: SHT2x_EID.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.0
+// PURPOSE: demo
+//     URL: https://github.com/RobTillaart/SHT2x
+
+
+#include "Wire.h"
+#include "SHT2x.h"
+
+uint32_t start;
+uint32_t stop;
+
+SHT2x sht;
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("SHT2x_LIB_VERSION: \t");
+  Serial.println(SHT2x_LIB_VERSION);
+
+  sht.begin();
+
+  uint32_t EIDA = sht.getEIDA();
+  Serial.print(EIDA, HEX);
+  Serial.println();
+  
+  uint32_t EIDB = sht.getEIDB();
+  Serial.print(EIDB, HEX);
+  Serial.println();
+  
+  uint8_t firmware = sht.getFirmwareVersion();
+  Serial.print(firmware, HEX);
+  Serial.println();
+
+  Serial.println("\n done...");
+}
+
+
+void loop()
+{
+
+}
+
+// -- END OF FILE --
